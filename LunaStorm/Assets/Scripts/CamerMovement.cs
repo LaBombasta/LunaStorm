ec70cb3a886eaffb9aac39f35e7adf753b90db15
DivineArchAngel <117623107+DivineArchAngel@users.noreply.github.com>
2024-04-08 22:40:19 -0400

    minor changes

---
+++ LunaStorm/Assets/Scripts/CamerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//to move forward a low speed untill spawn trigger collision, then lock camera until wave cleared
+
+public class CamerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LunaStorm/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//create portal effect to view next level, when trigger box collision, load next level
+
+public class LevelChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
