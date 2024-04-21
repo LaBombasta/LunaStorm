30e32463cb4de4d13b6679595590736942f15da9
James Colley <jcolleywork@gmail.com>
2024-04-21 00:08:45 -0700

    simple changes

--- LunaStorm/Assets/Scripts/UI/GameManager.cs
+++ LunaStorm/Assets/Scripts/UI/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            Debug.Log("you dont have teh script");
+            Debug.Log("you dont have the script");
         }
         
         
